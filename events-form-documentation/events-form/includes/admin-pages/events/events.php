6a0a4f82c3044641de540ebd562705164aa25f5d
GossenreiterThomas <t.gossenreiter@students.htl-leonding.ac.at>
2025-11-07 11:46:16 +0100

    shortcode design in development

--- events-form-documentation/events-form/includes/admin-pages/events/events.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('ABSPATH')) exit;
-
-global $wpdb;
-
-// Include modular files
-require_once plugin_dir_path(__FILE__) . 'add-event.php';
-require_once plugin_dir_path(__FILE__) . 'list-events.php';
-?>
